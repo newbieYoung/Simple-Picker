7656bf2ece66289b52ebc9948890d6e9a9c0f947
NewYoung <657851574@qq.com>
2019-08-19 17:21:57 +0800

    add eslint

--- index.js
+++ index.js
@@ -11,7 +11,7 @@
     }
 }(function (Prefix) {
     //兼容性处理
-    window.requestAnimFrame = (function() {
+    window.requestAnimFrame = (function () {
         return window.requestAnimationFrame ||
             window.mozRequestAnimationFrame ||
             window.webkitRequestAnimationFrame ||
@@ -23,21 +23,25 @@
     var transitionDurationPro = Prefix.prefix('transitionDuration');
 
     //常用方法名简化
-    function on (el, evt, callback) {
+    function on(el, evt, callback) {
         el.addEventListener(evt, callback);
     }
-    function off (el, evt, callback) {
+
+    function off(el, evt, callback) {
         el.removeEventListener(evt, callback);
     }
-    function qs (str) {
+
+    function qs(str) {
         return document.querySelector(str);
     }
-    function qsa (str) {
+
+    function qsa(str) {
         return document.querySelectorAll(str);
     }
-    function getIndex (val, array) {
+
+    function getIndex(val, array) {
         var res = 0;
-        for (var i = 0, len = array.length;i < len;i++) {
+        for (var i = 0, len = array.length; i < len; i++) {
             if (array[i].value === val) {
                 res = i;
             }
@@ -70,7 +74,7 @@
         this.onChangeEnd = config.onChangeEnd || function () {};
         this.onConfirm = config.onConfirm || function () {};
 
-        this.id = 'picker-'+new Date().getTime();
+        this.id = 'picker-' + new Date().getTime();
         this.itemHeight = config.itemHeight || 36;
         this.listHeight = config.listHeight || this.itemHeight * 6;
         this.topGapHeight = config.topGapHeight || 0;
@@ -81,27 +85,27 @@
         this.maskVisible = config.maskVisible != null ? config.maskVisible : true;
 
         this.init();
-    };
+    }
 
     //构建html结构
-    SimplePicker.prototype.constructor = function(){
+    SimplePicker.prototype.constructor = function () {
         var html = '';
         html += '<div class="picker-component">';
 
         //遮罩
-        if(this.maskVisible){
+        if (this.maskVisible) {
             html += '<div class="pk-cover"></div>';
         }
 
         html += '<div class="pk-target">';
 
         //头部按钮区域
-        if(this.headVisible){
+        if (this.headVisible) {
             html += '<div class="pt-header"><span class="ph-cancel">取消</span><span class="ph-confirm">确定</span></div>';
         }
 
         //头部占位元素
-        if(this.topGapHeight>0){
+        if (this.topGapHeight > 0) {
             html += '<div class="pt-top-gap"></div>';
         }
 
@@ -112,7 +116,7 @@
         html += '</div>';
 
         //底部占位元素
-        if(this.bottomGapHeight>0){
+        if (this.bottomGapHeight > 0) {
             html += '<div class="pt-bottom-gap"></div>';
         }
 
@@ -127,76 +131,76 @@
     };
 
     //初始化
-    SimplePicker.prototype.init = function(){
+    SimplePicker.prototype.init = function () {
         this.constructor();
-        this.$picker = qs('#'+this.id+' .pk-target');
+        this.$picker = qs('#' + this.id + ' .pk-target');
 
         //遮罩
-        if(this.maskVisible){
-            this.$cover = qs('#'+this.id+' .pk-cover');
+        if (this.maskVisible) {
+            this.$cover = qs('#' + this.id + ' .pk-cover');
         }
 
         //头部按钮区域
-        if(this.headVisible){
-            this.$cancel = qs('#'+this.id+' .ph-cancel');
-            this.$confirm = qs('#'+this.id+' .ph-confirm');
+        if (this.headVisible) {
+            this.$cancel = qs('#' + this.id + ' .ph-cancel');
+            this.$confirm = qs('#' + this.id + ' .ph-confirm');
             this.btnEvent();
         }
 
         //头部占位元素
-        if(this.topGapHeight>0){
-            this.$topGap = qs('#'+this.id+' .pt-top-gap');
-            this.$topGap.style.height = this.topGapHeight+'px';
+        if (this.topGapHeight > 0) {
+            this.$topGap = qs('#' + this.id + ' .pt-top-gap');
+            this.$topGap.style.height = this.topGapHeight + 'px';
         }
 
         //底部占位元素
-        if(this.bottomGapHeight>0){
-            this.$bottomGap = qs('#'+this.id+' .pt-bottom-gap');
-            this.$bottomGap.style.height = this.bottomGapHeight+'px';
+        if (this.bottomGapHeight > 0) {
+            this.$bottomGap = qs('#' + this.id + ' .pt-bottom-gap');
+            this.$bottomGap.style.height = this.bottomGapHeight + 'px';
         }
 
         //设置选项列表高度
-        this.$list = qs('#'+this.id+' .pt-list');
-        this.$list.style.height = this.listHeight+'px';
+        this.$list = qs('#' + this.id + ' .pt-list');
+        this.$list.style.height = this.listHeight + 'px';
 
         //设置上下遮罩高度
-        this.$topCover = qs('#'+this.id+' .pl-top-cover');
-        this.$bottomCover = qs('#'+this.id+' .pl-bottom-cover');
-        var coverHeight = (this.listHeight - this.itemHeight)/2;
-        this.$topCover.style.height = coverHeight+'px';
-        this.$bottomCover.style.height = coverHeight+'px';
+        this.$topCover = qs('#' + this.id + ' .pl-top-cover');
+        this.$bottomCover = qs('#' + this.id + ' .pl-bottom-cover');
+        var coverHeight = (this.listHeight - this.itemHeight) / 2;
+        this.$topCover.style.height = coverHeight + 'px';
+        this.$bottomCover.style.height = coverHeight + 'px';
 
         //设置默认值偏移
-        this.$colParents = qsa('#'+this.id+' .pl-col');
-        this.$cols = qsa('#'+this.id+' .pl-col ul');
-        for(var i=0;i<this.data.length;i++){
+        this.$colParents = qsa('#' + this.id + ' .pl-col');
+        this.$cols = qsa('#' + this.id + ' .pl-col ul');
+        for (var i = 0; i < this.data.length; i++) {
             var offsetHeight = getIndex(this.default[i], this.data[i]) * this.itemHeight;
-            this.setOffset(i,offsetHeight);
-            this.moveListener(i,offsetHeight);//初始化滑动监听
+            this.setOffset(i, offsetHeight);
+            this.moveListener(i, offsetHeight); //初始化滑动监听
         }
     };
 
     //渲染选项列表数据
-    SimplePicker.prototype.renderCols = function(){
+    SimplePicker.prototype.renderCols = function () {
         var html = '';
         var cssTexts = [];
-        if(this.itemStyle){
-            for(var i=0;i<this.itemStyle.length;i++){
+        if (this.itemStyle) {
+            for (var i = 0; i < this.itemStyle.length; i++) {
                 cssTexts[i] = '';
                 var styles = this.itemStyle[i];
-                for(var j = 0;j<styles.length;j++){
-                    cssTexts[i] += styles[j].property+':'+styles[j].value+';';
+                for (var j = 0; j < styles.length; j++) {
+                    cssTexts[i] += styles[j].property + ':' + styles[j].value + ';';
                 }
             }
         }
 
-        for(var i=0;i<this.data.length;i++){
+        for (var i = 0; i < this.data.length; i++) {
             var col = this.data[i];
             var cssText = cssTexts[i] || '';
             html += '<div class="pl-col">';
             html += '<ul>';
-            for(var j=0;j<col.length;j++){
-                html += '<li style="'+cssText+'">'+col[j].title+'</li>'
+            for (var j = 0; j < col.length; j++) {
+                html += '<li style="' + cssText + '">' + col[j].title + '</li>'
             }
             html += '</ul>';
             html += '</div>';
@@ -206,27 +210,27 @@
     };
 
     //设置列表选项高度
-    SimplePicker.prototype.setListHeight = function(height){
+    SimplePicker.prototype.setListHeight = function (height) {
         this.listHeight = height;
-        this.$list.style.height = this.listHeight+'px';
+        this.$list.style.height = this.listHeight + 'px';
 
         //重置上下cover高度
-        var coverHeight = (this.listHeight - this.itemHeight)/2;
-        this.$topCover.style.height = coverHeight+'px';
-        this.$bottomCover.style.height = coverHeight+'px';
+        var coverHeight = (this.listHeight - this.itemHeight) / 2;
+        this.$topCover.style.height = coverHeight + 'px';
+        this.$bottomCover.style.height = coverHeight + 'px';
 
         //重置结果偏移高度
-        for(var i=0;i<this.data.length;i++){
+        for (var i = 0; i < this.data.length; i++) {
             var offsetHeight = getIndex(this.result[i], this.data[i]) * this.itemHeight;
-            this.setOffset(i,offsetHeight);
+            this.setOffset(i, offsetHeight);
         }
     };
 
     //显示
-    SimplePicker.prototype.show = function(){
+    SimplePicker.prototype.show = function () {
         var self = this;
         setTimeout(function () {
-            if(self.maskVisible){
+            if (self.maskVisible) {
                 self.$cover.classList.add('pc-show');
             }
             self.$picker.classList.add('pc-show');
@@ -234,42 +238,42 @@
     };
 
     //隐藏
-    SimplePicker.prototype.hide = function(){
-        if(this.maskVisible){
+    SimplePicker.prototype.hide = function () {
+        if (this.maskVisible) {
             this.$cover.classList.remove('pc-show');
         }
         this.$picker.classList.remove('pc-show');
     };
 
     //计算选取结果
-    SimplePicker.prototype.getResult = function(translateY){
-        var initOffset = (this.listHeight - this.itemHeight)/2;
-        var startIndex = parseInt(initOffset/this.itemHeight);
+    SimplePicker.prototype.getResult = function (translateY) {
+        var initOffset = (this.listHeight - this.itemHeight) / 2;
+        var startIndex = parseInt(initOffset / this.itemHeight);
         var startOffset = initOffset - this.itemHeight * startIndex;
-        var result = startIndex - (translateY - startOffset)/this.itemHeight;
+        var result = startIndex - (translateY - startOffset) / this.itemHeight;
         return result;
     };
 
     //设置偏移
-    SimplePicker.prototype.setOffset = function(colNo,offsetHeight,touchEndDuration){
+    SimplePicker.prototype.setOffset = function (colNo, offsetHeight, touchEndDuration) {
         //过渡时长最长不超过 0.4 秒，最短不小于 0.2 秒
         var minDuration = 0.2;
         var maxDuration = 0.4;
         var time = touchEndDuration > 0 ? touchEndDuration : 0;
-        if(time>0){
-            if(time>maxDuration){
+        if (time > 0) {
+            if (time > maxDuration) {
                 time = maxDuration;
-            }else if(time<minDuration){
+            } else if (time < minDuration) {
                 time = minDuration;
             }
         }
 
         var el = this.$cols[colNo];
         var translateY = this.listHeight / 2 - this.itemHeight / 2 - offsetHeight;
-        el.style[transitionDurationPro] = time+'s';
-        el.style[transformProperty] = 'translateY('+translateY+'px)';
+        el.style[transitionDurationPro] = time + 's';
+        el.style[transformProperty] = 'translateY(' + translateY + 'px)';
 
-        if(time > 0){
+        if (time > 0) {
             var lineNo = this.getResult(translateY);
             this.result[colNo] = this.data[colNo][lineNo].value;
             this.onChangeEnd(this.result);
@@ -277,7 +281,7 @@
     };
 
     //获取结束偏移距离
-    SimplePicker.prototype.getEndOffset = function(offset){
+    SimplePicker.prototype.getEndOffset = function (offset) {
         //超过一半进一，没超过舍去
         offset = offset % this.itemHeight > this.itemHeight / 2 ? Math.ceil(offset / this.itemHeight) * this.itemHeight : Math.floor(offset / this.itemHeight) * this.itemHeight;
         return offset;
@@ -287,52 +291,52 @@
     SimplePicker.prototype.btnEvent = function () {
         var self = this;
 
-        on(this.$cancel, 'click', function(){//取消
+        on(this.$cancel, 'click', function () { //取消
             self.hide();
         });
 
-        on(this.$confirm, 'click', function(){//确定
+        on(this.$confirm, 'click', function () { //确定
             self.hide();
             self.onConfirm(self.result);
         });
     };
 
     //滑动监听
-    SimplePicker.prototype.moveListener = function(colNo,offsetHeight){
+    SimplePicker.prototype.moveListener = function (colNo, offsetHeight) {
         var self = this;
-        var touchStartY = 0;//触摸开始Y值
-        var touchStartTime = 0;//触摸开始时间
-        var touchMovedY = offsetHeight;//偏移距离，默认为初始偏移距离
-        var curColNum = colNo;//当前滑动行序号
+        var touchStartY = 0; //触摸开始Y值
+        var touchStartTime = 0; //触摸开始时间
+        var touchMovedY = offsetHeight; //偏移距离，默认为初始偏移距离
+        var curColNum = colNo; //当前滑动行序号
         var element = this.$colParents[curColNum];
-        var stopInertiaMove = false;//是否停止惯性移动
+        var stopInertiaMove = false; //是否停止惯性移动
 
         //触摸开始
-        function touchstartHandler (e) {
+        function touchstartHandler(e) {
             touchStartY = e.touches[0].pageY;
             touchStartTime = new Date().getTime();
             stopInertiaMove = true;
         }
 
         //触摸移动
-        function touchmoveHandler (e) {
+        function touchmoveHandler(e) {
             e.preventDefault();
             var touchMoveY = e.touches[0].pageY;
-            var _touchMovedY = touchStartY - touchMoveY + touchMovedY;//当前移动偏移距离
+            var _touchMovedY = touchStartY - touchMoveY + touchMovedY; //当前移动偏移距离
             self.setOffset(curColNum, _touchMovedY);
             stopInertiaMove = true;
         }
 
         //触摸结束
-        function touchendtHandler (e) {
+        function touchendtHandler(e) {
             var toucheEndY = e.changedTouches[0].pageY;
-            touchMovedY = touchStartY - toucheEndY + touchMovedY;//最终偏移距离
+            touchMovedY = touchStartY - toucheEndY + touchMovedY; //最终偏移距离
 
             //惯性移动
             stopInertiaMove = false;
             var nowTime = new Date().getTime();
             var vec = (toucheEndY - touchStartY) / (nowTime - touchStartTime); //惯性滑动平均速度
-            var initVec = vec * 100;//毫秒速度转换为秒速度
+            var initVec = vec * 100; //毫秒速度转换为秒速度
 
             //越界
             if (touchMovedY < 0) {
@@ -342,14 +346,14 @@
                 return;
             }
             if (touchMovedY > self.data[curColNum].length * self.itemHeight - self.itemHeight) {
-                var maxMovedY = self.data[curColNum].length * self.itemHeight - self.itemHeight;//最大偏移距离
+                var maxMovedY = self.data[curColNum].length * self.itemHeight - self.itemHeight; //最大偏移距离
                 var duration = Math.abs((touchMovedY - maxMovedY) / initVec);
                 touchMovedY = maxMovedY;
                 self.setOffset(curColNum, maxMovedY, duration);
                 return;
             }
 
-            function inertiaMove (curstamp,laststamp) {
+            function inertiaMove(curstamp, laststamp) {
                 if (stopInertiaMove) {
                     return;
                 }
@@ -363,7 +367,7 @@
                         return;
                     }
                     if (touchMovedY > self.data[curColNum].length * self.itemHeight - self.itemHeight) {
-                        var maxMovedY = self.data[curColNum].length * self.itemHeight - self.itemHeight;//最大偏移距离
+                        var maxMovedY = self.data[curColNum].length * self.itemHeight - self.itemHeight; //最大偏移距离
                         var duration = Math.abs((touchMovedY - maxMovedY) / initVec);
                         touchMovedY = maxMovedY;
                         self.setOffset(curColNum, maxMovedY, duration);
@@ -373,20 +377,20 @@
                     //回归
                     var cacheMovedY = touchMovedY;
                     touchMovedY = self.getEndOffset(touchMovedY);
-                    var duration = Math.abs((touchMovedY - cacheMovedY)/initVec);
+                    var duration = Math.abs((touchMovedY - cacheMovedY) / initVec);
                     self.setOffset(curColNum, touchMovedY, duration);
                     return;
                 }
                 touchMovedY += dist;
                 self.setOffset(curColNum, touchMovedY);
-                vec /= 1.1;//惯性移动阻力系数
+                vec /= 1.1; //惯性移动阻力系数
 
-                window.requestAnimFrame(function(timestamp){
-                    inertiaMove(timestamp,curstamp);
+                window.requestAnimFrame(function (timestamp) {
+                    inertiaMove(timestamp, curstamp);
                 });
             }
-            window.requestAnimFrame(function(timestamp){
-                inertiaMove(timestamp,timestamp)
+            window.requestAnimFrame(function (timestamp) {
+                inertiaMove(timestamp, timestamp)
             });
         }
 
